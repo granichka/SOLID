36dcfc1c982eb8cba1a63ee40b5b681cf4fee384
rs <rs@MacBook-Air-rs.local>
2020-06-29 19:08:14 +0300

    AbstractTechniqueService.java WAS CHANGED. EXTENDING OF AbstractService WAS ADDED

--- src/main/java/local/nix/solid/project/gym/service/AbstractTechniqueService.java
+++ src/main/java/local/nix/solid/project/gym/service/AbstractTechniqueService.java
@@ -2,7 +2,7 @@ package local.nix.solid.project.gym.service;
 
 import local.nix.solid.project.gym.data.AbstractTechnique;
 
-public interface AbstractTechniqueService<T extends AbstractTechnique> {
+public interface AbstractTechniqueService<T extends AbstractTechnique> extends AbstractService{
 
     void turnOn();
     void turnOff();
