0b1d8bab1421aa60d3a90342851abcad793feefd
rs <rs@MacBook-Air-rs.local>
2020-06-29 18:04:04 +0300

    AbstractTechnique.java WAS CHANGED. <T> WAS ADDED

--- src/main/java/local/nix/solid/project/gym/AirConditioning.java
+++
@@ -1,4 +0,0 @@
-package local.nix.solid.project.gym;
-
-public class AirConditioning extends AbstractTechnique{
-}
--- src/main/java/local/nix/solid/project/gym/data/AbstractTechnique.java
+++ src/main/java/local/nix/solid/project/gym/data/AbstractTechnique.java
@@ -1,5 +1,5 @@
 package local.nix.solid.project.gym.data;
 
-public abstract class AbstractTechnique extends AbstractData<AbstractTechnique>{
+public abstract class AbstractTechnique<T> extends AbstractData<T>{
 
 }
