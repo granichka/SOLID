d98c84e04c140129b625cf52e7d79cb41d527a26
rs <rs@MacBook-Air-rs.local>
2020-06-29 20:29:44 +0300

    CoachResponsibility WAS RENAMED TO CoachResponsibilityServiceImpl. TWO PRIVATE FIELDS airConditioningService AND treadmillService WEREADDED

--- src/main/java/local/nix/solid/project/gym/service/impl/CoachResponsibilityServiceImpl.java
+++ src/main/java/local/nix/solid/project/gym/service/impl/CoachResponsibilityServiceImpl.java
@@ -1,18 +1,23 @@
-package local.nix.solid.project.gym;
+package local.nix.solid.project.gym.service.impl;
 
 import local.nix.solid.project.gym.data.AirConditioning;
 import local.nix.solid.project.gym.data.TreadMill;
 import local.nix.solid.project.gym.service.AirConditioningService;
+import local.nix.solid.project.gym.service.CoachResponsibilityService;
 import local.nix.solid.project.gym.service.TreadMillService;
 
-public class CoachResponsibility {
+public class CoachResponsibilityServiceImpl implements CoachResponsibilityService {
 
-    public void startWorkOutResponsibility(AirConditioningService airConditioning, TreadMillService treadMill) {
-                airConditioning.turnOn();
-                treadMill.turnOn();
+    private AirConditioningService airConditioningService = new AirConditioningServiceImpl();
+    private TreadMillService treadMillService = new TreadMillServiceImpl();
+
+    public void startWorkOutResponsibility() {
+        airConditioningService.turnOn();
+        treadMillService.turnOn();
     }
 
     public void finishWorkOutResponsibility() {
         System.out.println("Убирает за клиентами спортивное оборудование");
     }
+
 }
