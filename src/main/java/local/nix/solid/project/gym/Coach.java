b9f4e06eae97fe5c5246f8d5ad4ee67b510a51e8
rs <rs@MacBook-Air-rs.local>
2020-06-29 16:30:10 +0300

    train METHOD WAS CREATED. PART OF startWorkOut METHOD WAS MOVED TO NEW train METHOD

--- src/main/java/local/nix/solid/project/gym/Coach.java
+++ src/main/java/local/nix/solid/project/gym/Coach.java
@@ -5,8 +5,14 @@ public class Coach {
     public void startWorkOut(Client client) {
         System.out.println("Включает беговую дорожку");
         System.out.println("Включает кондиционер");
-        System.out.println("Начинает тренировку");
+        train(client);
         System.out.println("Убирает за кем-то спортивное оборудование");
+
+
+    }
+
+    public void train(Client client) {
+        System.out.println("Начинает тренировку");
         System.out.println("Следит за корректностью выполнения упражнений");
         System.out.println("Страхует");
         System.out.println("Заканчивает тренировку");
