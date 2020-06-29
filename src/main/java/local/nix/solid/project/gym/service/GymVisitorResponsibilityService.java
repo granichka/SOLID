7903d524a9f95da5ca5ae0c1970488cc76cca65c
rs <rs@MacBook-Air-rs.local>
2020-06-29 20:24:24 +0300

    CoachResponsibilityService.java WAS ADDED

---
+++ src/main/java/local/nix/solid/project/gym/service/GymVisitorResponsibilityService.java
@@ -0,0 +1,6 @@
+package local.nix.solid.project.gym.service;
+
+import local.nix.solid.project.gym.data.abstr.GymVisitor;
+
+public interface GymVisitorResponsibilityService<T extends GymVisitor> extends AbstractService{
+}
