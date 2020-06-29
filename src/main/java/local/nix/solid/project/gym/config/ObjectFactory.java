9bbef8a22112f99306edfdfe688de9582ecf8b94
rs <rs@MacBook-Air-rs.local>
2020-06-29 23:00:32 +0300

    config FIELD WAS ADDED. INITIALIZATION OF consig WAS ADDED TO CONSTRUCTOR. createObject METHOD WAS ADDED.

--- src/main/java/local/nix/solid/project/gym/config/ObjectFactory.java
+++ src/main/java/local/nix/solid/project/gym/config/ObjectFactory.java
@@ -1,12 +1,25 @@
 package local.nix.solid.project.gym.config;
 
 
+import local.nix.solid.project.gym.config.impl.JavaApplicationConfiguration;
+import local.nix.solid.project.gym.service.CoachResponsibilityService;
+import local.nix.solid.project.gym.service.impl.MainCoachResponsibilityServiceImpl;
+
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ObjectFactory {
 
     private static ObjectFactory instance;
+    private ApplicationConfiguration config;
 
-
-    private ObjectFactory() { }
+    private ObjectFactory() {
+        config = new JavaApplicationConfiguration(
+                "local.nix.solid.project.gym",
+                new HashMap<>(Map.of(CoachResponsibilityService.class, MainCoachResponsibilityServiceImpl.class)));
+    }
 
     public static ObjectFactory getInstance() {
         if (instance == null) {
@@ -14,4 +27,19 @@ public class ObjectFactory {
         }
         return instance;
     }
+
+    public <T> T createObject(Class<T> type) {
+        Class<? extends T> implClass = type;
+        if (type.isInterface()) {
+            implClass = config.getCurrentImplementation(type);
+        }
+        T t;
+        try {
+            t = implClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException("невозможно создать класс: " + e.getClass().getName() + " " + ",msg: " + e.getMessage());
+        }
+        return t;
+    }
+
 }
