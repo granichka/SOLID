4c96716fc1b876909584f8791c2c4d56ade75281
rs <rs@MacBook-Air-rs.local>
2020-06-29 22:18:30 +0300

    JavaApplicationConfiguration.java WAS MODIFIED. scanner AND mapImpl FIELDS WERE ADDED. CONSTRUCTOR WAS ADDED. getCurrentImplementation METHOD WAS CHANGED

--- src/main/java/local/nix/solid/project/gym/config/impl/JavaApplicationConfiguration.java
+++ src/main/java/local/nix/solid/project/gym/config/impl/JavaApplicationConfiguration.java
@@ -1,11 +1,29 @@
 package local.nix.solid.project.gym.config.impl;
 
 import local.nix.solid.project.gym.config.ApplicationConfiguration;
+import org.reflections.Reflections;
+
+import java.util.Map;
+import java.util.Set;
 
 public class JavaApplicationConfiguration implements ApplicationConfiguration {
 
+    private Reflections scanner;
+    private Map<Class, Class> mapImpl;
+
+    public JavaApplicationConfiguration(String packageToScan, Map<Class, Class> mapImpl) {
+        this.scanner = new Reflections(packageToScan);
+        this.mapImpl = mapImpl;
+    }
+
     @Override
     public <T> Class<? extends T> getCurrentImplementation(Class<T> type) {
-        return null;
+        return mapImpl.computeIfAbsent(type, aClass -> {
+            Set<Class<? extends T>> children = scanner.getSubTypesOf(type);
+            if (children.size() != 1) {
+                throw new RuntimeException("может быть только одна имплементация");
+            }
+            return children.iterator().next();
+        });
     }
 }
