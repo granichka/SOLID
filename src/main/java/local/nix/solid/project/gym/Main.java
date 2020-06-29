b9336f0c864cf4bd574c683c87abeeedb7a21a02
rs <rs@MacBook-Air-rs.local>
2020-06-29 20:34:45 +0300

    CoachService TYPE OF OBJECT WAS REFACTORED TO CoachServiceImpl

--- src/main/java/local/nix/solid/project/gym/Main.java
+++ src/main/java/local/nix/solid/project/gym/Main.java
@@ -1,9 +1,12 @@
 package local.nix.solid.project.gym;
 
+import local.nix.solid.project.gym.data.Client;
+import local.nix.solid.project.gym.service.impl.CoachServiceImpl;
+
 public class Main {
 
     public static void main(String[] args) {
-        CoachService coachService = new CoachService();
-        coachService.startWorkOut(new Client());
+        CoachServiceImpl coachServiceImpl = new CoachServiceImpl();
+        coachServiceImpl.startWorkOut(new Client());
     }
 }
