a7071248574e67c2cbcf458d131bec458410d161
rs <rs@MacBook-Air-rs.local>
2020-06-29 18:22:36 +0300

    AirConditioningService.java AND TreadMillService.java WERE ADDED

---
+++ src/main/java/local/nix/solid/project/gym/service/AirConditioningService.java
@@ -0,0 +1,6 @@
+package local.nix.solid.project.gym.service;
+
+import local.nix.solid.project.gym.data.AirConditioning;
+
+public interface AirConditioningService extends AbstractTechniqueService<AirConditioning>{
+}
---
+++ src/main/java/local/nix/solid/project/gym/service/TreadMillService.java
@@ -0,0 +1,6 @@
+package local.nix.solid.project.gym.service;
+
+import local.nix.solid.project.gym.data.TreadMill;
+
+public interface TreadMillService extends AbstractTechniqueService<TreadMill>{
+}
